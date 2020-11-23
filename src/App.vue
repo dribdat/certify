18d05e003c4cb2e24d2dac84088b94777f0d17d5
Oleg Lavrovsky <oleg@datalets.ch>
2020-11-23 20:41:48 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="card">
+        <div class="card-image mt-5">
+          <img
+            alt="Logo"
+            src="https://make.opendata.ch/files/2013/01/Make.OpenData.ch-Logo1.png"
+          />
+        </div>
+        <div class="card-content">
+          <div class="content has-text-left">
+            <GetCertified
+              baseUrl="https://bucketeer-e34189e0-4cb3-496d-a72e-2dcf17cab858.s3-eu-west-1.amazonaws.com/public/certs/oedh2020"
+            />
+          </div>
+        </div>
+        <footer class="card-footer">
+          <p class="card-footer-item">
+            <span>
+              Follow on
+              <a href="https://twitter.com/opendatach" target="_blank"
+                >Twitter</a
+              >
+            </span>
+          </p>
+          <p class="card-footer-item">
+            <span>
+              Like on
+              <a href="https://facebook.com/opendatach" target="_blank"
+                >Facebook</a
+              >
+            </span>
+          </p>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GetCertified from "./components/GetCertified";
+
+export default {
+  name: "App",
+  components: {
+    GetCertified,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.card {
+  max-width: 600px;
+}
+</style>
---
+++ src/components/GetCertified.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="getCertified">
+    <section class="preview has-text-centered">
+      <div class="box" style="display: inline-block">
+        <img :src="previewUrl" />
+      </div>
+    </section>
+    <section class="form" v-show="!isDownloading">
+      <b-field label="First Name">
+        <b-input v-model="first_name"></b-input>
+      </b-field>
+
+      <b-field label="Last Name">
+        <b-input v-model="last_name"></b-input>
+      </b-field>
+
+      <div class="field">
+        <b-checkbox v-model="honestyPolicy">
+          <b
+            >I took part in the hackathon and attest to my honest
+            contribution</b
+          >
+          in agreement with the
+          <a href="https://hackcodeofconduct.org/" target="_blank"
+            >Hack Code of Conduct</a
+          >
+          during the event, knowing that the organizers fully reserve the right
+          to withdraw their recognition of my participation in case of
+          misconduct.
+        </b-checkbox>
+      </div>
+
+      <div class="field has-text-centered m-5">
+        <button
+          class="button is-primary"
+          :disabled="!honestyPolicy"
+          @click="getCertified"
+        >
+          Get certificate
+        </button>
+      </div>
+    </section>
+
+    <section class="confirm m-5" v-show="isDownloading">
+      <p>
+        Great! Your certificate is ready:
+      </p>
+      <a :href="downloadUrl" download>
+        <button class="button is-success">&blacktriangledown; Download PDF</button></a>
+    </section>
+
+    <section class="confirm" v-show="isErrored">
+      <b-notification type="is-warning" :closable="false">
+        There was an error. Please try correcting the spelling of your first or
+        last name, which should match your answers in the registration form.
+        Contact the organizing team via Slack or
+        <a href="mailto:info@opendata.ch?subject=Certificate">E-mail</a>
+        if you continue having an issue.
+      </b-notification>
+    </section>
+    <section class="info" v-show="!isErrored">
+      <b-notification type="is-info" :closable="false">
+        If you have any questions, please contact the organizing team via Slack
+        or
+        <a href="mailto:info@opendata.ch?subject=Certificate">E-mail</a>.
+      </b-notification>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    baseUrl: String,
+  },
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      honestyPolicy: false,
+      isDownloading: false,
+      downloadUrl: null,
+      previewUrl: null,
+      isErrored: false,
+    };
+  },
+  methods: {
+    getCertified: function () {
+      // build download link
+      this.downloadUrl = [this.first_name, this.last_name, ".pdf"].join("");
+      this.downloadUrl = [this.baseUrl, this.downloadUrl].join("/");
+      // check if the link resolves
+      this.isErrored = false;
+      axios
+        .get(this.downloadUrl)
+        .then((res) => {
+          // replace form with confirmation
+          this.isDownloading = true;
+        })
+        .catch((err) => {
+          // show error message
+          this.isErrored = true;
+        });
+    },
+  },
+  mounted() {
+    this.previewUrl = this.baseUrl + "/preview.png";
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.preview img {
+  width: 200px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
